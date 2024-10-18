fd0e2e041767c612a55fb5e1f29235a2f5a56f40
tlsstern <thomas.stern@noseryoung.com>
2024-10-18 14:30:43 +0200

    Added Subtraction and division and added failsafe so it will go back if wrong number has been pressed

--- 2024/Python/Aufgaben/01_Aufgabe/python.py
+++ 2024/Python/Aufgaben/01_Aufgabe/python.py
@@ -1,17 +1,91 @@
 def calculator():
-    modifier = input("What modifier do you want to use? [+][-][/][*][^][Wurzel]")
+    modifier = input("What modifier do you want to use? [+][-][/][*][^][Wurzel]\n")
     if modifier == "+":
-        How_many = int(input("How many numbers do you want to calculate toegther?"))
-        i = 0
-        sum = 0
-        while(i < How_many):
-            num1 = int(input("Enter a number"))
-            print(sum)
-            sum = sum + num1
-            print(sum)
-            i += 1
-        print("Your sum is", sum)
+        addition()
+    elif modifier == "-":
+        subtraction()
+    elif modifier == "/":
+        division()
+    elif modifier == "*":
+        multiplication()
+    elif modifier == "^":
+        power()
+    elif modifier == "Wurzel" or modifier == "wurzel":  # Corrected the condition here
+        root()
+    else:
+        print("Invalid modifier")
+        calculator()
 
+def addition():
+    while True:
+        try:
+            How_many = int(input("How many numbers do you want to calculate together?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    i = 0
+    sum = 0
+    while(i < How_many):
+        while True:
+            try:
+                num1 = int(input("Enter a number\n"))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+        sum += num1
+        i += 1
+    print("Your sum is", sum)
 
+def subtraction():
+    while True:
+        try:
+            How_many = int(input("How many numbers do you want to calculate together?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    i = 0
+    while True:
+        try:
+            result = int(input("What would you want to be your first number?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    print("Starting with:", result)
+    while i < How_many - 1:
+        while True:
+            try:
+                num = int(input("Enter a number to subtract\n"))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+        result -= num
+        i += 1
+    print("Your result is", result)
+
+def division():
+    while True:
+        try:
+            How_many = int(input("How many numbers do you want to subtract from the first number?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    i = 0
+    while True:
+        try:
+            result = int(input("What would you want to be your first number?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    print("Starting with:", result)
+    while i < How_many:
+        while True:
+            try:
+                num = int(input("Enter a number to divide\n"))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+        result = result / num
+        i += 1
+    print("Your result is", result)
 
 calculator()
