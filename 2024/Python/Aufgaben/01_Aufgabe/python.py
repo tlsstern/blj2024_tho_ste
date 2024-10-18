9c1e106b42eaa639b53e9286d4a53f4c81bee296
tlsstern <thomas.stern@noseryoung.com>
2024-10-18 14:48:32 +0200

    Finished Calculator and bug checked

--- 2024/Python/Aufgaben/01_Aufgabe/python.py
+++ 2024/Python/Aufgaben/01_Aufgabe/python.py
@@ -35,6 +35,7 @@ def addition():
         sum += num1
         i += 1
     print("Your sum is", sum)
+    play_again()
 
 def subtraction():
     while True:
@@ -61,6 +62,7 @@ def subtraction():
         result -= num
         i += 1
     print("Your result is", result)
+    play_again()
 
 def division():
     while True:
@@ -87,5 +89,68 @@ def division():
         result = result / num
         i += 1
     print("Your result is", result)
+    play_again()
+
+def multiplication():
+    while True:
+        try:
+            How_many = int(input("How many numbers do you want to multiply together?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    i = 0
+    result = 1
+    while(i < How_many):
+        while True:
+            try:
+                num1 = int(input("Enter a number\n"))
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+        result = result * num1
+        i += 1
+    print("Your result is", result)
+    play_again()
+
+def power():
+    while True:
+        result = 0
+        try:
+            first_number = int(input("Whats your first number?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    while True:
+        try:
+            exponent = int(input("Whats your exponent?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    result = first_number ** exponent
+
+    print("Your result is", result)
+    play_again()
+
+def root():
+    while True:
+        result = 0
+        try:
+            first_number = int(input("Of which number do you want to find out the root?\n"))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    result = first_number **(1/2)
+
+    print("Your result is", result)
+    play_again()
+
+def play_again():
+    answer = input("Do you want to play again?\nPress 1 if yes\nPress anything if no\n")
+    if answer == 1:
+        calculator()
+    else:
+        exit(1)
+
 
 calculator()
