4a33f97a7f2e0b38d6f772a2837b956f0e0b6930
tlsstern <thomas.stern@noseryoung.com>
2024-10-21 13:24:27 +0200

    Finished the code for the console print and started with the second which goes into the index.html

--- 2024/Andere_Aufgaben_in_C/Table/main.c
+++ 2024/Andere_Aufgaben_in_C/Table/main.c
@@ -1,14 +1,85 @@
 #include <stdio.h>
 
+void basis_html();
+void table_html();
+
 int main()
 {
-    FILE *file = fopen("table.html", "w");
+    table_html();
+    // basis_html();
+}
 
-    fprintf(file, "<!DOCTYPE html><html><head><title>Table</title></head>");
-    fprintf(file, "<body>");
-    fprintf(file, "<!-- Content here -->");
-    fprintf(file, "</body></html>");
+void table_html() {
+    FILE *file = fopen("../index.html", "w");
+
+    printf("How many Colums do you want?\n");
+    int colums;
+    scanf("%d", &colums);
+    printf("How many Rows do you want?\n");
+    int row;
+    scanf("%d", &row);
+
+    fprintf(file,"\n\n\n\n\n\n");
+    fprintf(file,"<!DOCTYPE html>\n<html>\n<head>\n\t<title>Table</title>\n</head>\n");
+    fprintf(file,"<body>\n");
+    fprintf(file,"\t<!-- Content here -->\n");
+    fprintf(file,"\t<table>\n");
+
+    for(int i = 1; i <= row; i++) {
+        fprintf(file,"\t\t<tr>\n");
+        if (i == 1) {
+            for(int j = 1; j <= colums; j++) {
+                fprintf(file,"\t\t\t<td>\n");
+                fprintf(file,"\t\t\t\t<h1>Header %d<h1>\n", j);
+                fprintf(file,"\t\t\t</td>\n");
+            }
+        }else {
+            for(int j = 1; j <= colums; j++) {
+                fprintf(file,"\t\t\t<td>\n");
+                fprintf(file,"\t\t\t\tHello World\n");
+                fprintf(file,"\t\t\t</td>\n");
+            }
+
+        }
+
+        fprintf(file,"\t\t</tr>\n");
 
 
-    fclose(file);
+        fclose(file);
+    }
+}
+void basis_html() {
+    printf("How many Colums do you want?\n");
+    int colums;
+    scanf("%d", &colums);
+    printf("How many Rows do you want?\n");
+    int row;
+    scanf("%d", &row);
+
+    printf("\n\n\n\n\n\n");
+    printf("<!DOCTYPE html>\n<html>\n<head>\n\t<title>Table</title>\n</head>\n");
+    printf("<body>\n");
+    printf("\t<!-- Content here -->\n");
+    printf("\t<table>\n");
+
+    for(int i = 1; i <= row; i++) {
+        printf("\t\t<tr>\n");
+        if (i == 1) {
+            for(int j = 1; j <= colums; j++) {
+                printf("\t\t\t<td>\n");
+                printf("\t\t\t\t<h1>Header %d<h1>\n", j);
+                printf("\t\t\t</td>\n");
+            }
+        }else {
+            for(int j = 1; j <= colums; j++) {
+                printf("\t\t\t<td>\n");
+                printf("\t\t\t\tHello World\n");
+                printf("\t\t\t</td>\n");
+            }
+        }
+
+        printf("\t\t</tr>\n");
+    }
+    printf("\t</table>\n");
+    printf("</body>\n</html>\n");
 }
