c824e5b0ad7051d67429f3276839a170d99602a1
tlsstern <thomas.stern@noseryoung.com>
2024-10-24 11:35:10 +0200

    Added a failsafe, so if you entnersomething not good it goes back and asks again

--- 2024/Python/Aufgaben/01_Calculator/binary_translater.py
+++ 2024/Python/Aufgaben/01_Calculator/binary_translater.py
@@ -1,37 +1,53 @@
 from unicodedata import decimal
 
 def play_again():
-    playagain = input("Do you want to play again? [Y/N]")
-    if playagain == "Y" or playagain == "y":
-        print("\n\n")
-        choice()
-    else:
-        exit(1)
+    while True:
+        playagain = input("Do you want to play again? [Y/N] ")
+        if playagain.upper() == "Y":
+            print("\n\n")
+            choice()
+            break
+        elif playagain.upper() == "N":
+            exit(1)
+        else:
+            print("Invalid input. Please enter Y or N.")
 
 def choice():
     print("Do you want to:\n1. Integer to Binary\n2. Binary to Integer\n3. Exit")
-    choice = int(input(""))
-    if choice == 1:
-        decToBin()
-    elif choice == 2:
-        BinToDec()
-    else:
-        exit(1)
+    while True:
+        try:
+            choice = int(input(""))
+            if choice == 1:
+                decToBin()
+                break
+            elif choice == 2:
+                BinToDec()
+                break
+            elif choice == 3:
+                exit(1)
+            else:
+                print("Invalid choice. Please enter 1, 2, or 3.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 def decToBin():
     i = 0
     binary = []
-    num = int(input("What number do you want to have as an Binary? "))
-    if num == 0:
-        print("The binary of 0 is 0")
+    try:
+        num = int(input("What number do you want to have as Binary? "))
+        if num == 0:
+            print("The binary of 0 is 0")
+            play_again()
+        while(num > 0):
+            binary.append(num % 2)
+            num = num // 2
+            i += 1
+        binary.reverse()
+        print("The binary number is: ", *binary, sep="")
         play_again()
-    while(num > 0):
-        binary.append(num % 2)
-        num = num // 2
-        i += 1
-    binary.reverse()
-    print("The binary number is: ", *binary, sep="")
-    play_again()
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+        decToBin()
 
 def BinToDec():
     i = 0
@@ -46,9 +62,10 @@ def BinToDec():
             length -= 1
         else:
             print("This is not a binary number!")
+            BinToDec()
+            return
     num = num / 2
     print(num)
     play_again()
-
 
 choice()
