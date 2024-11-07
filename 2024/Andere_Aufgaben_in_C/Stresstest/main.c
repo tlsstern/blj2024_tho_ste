edf56e8cf34c81178218f5d915320858e6e1c853
tlsstern <thomas.stern@noseryoung.com>
2024-11-07 10:20:58 +0100

    Stresstest erstellt

--- 2024/Andere_Aufgaben_in_C/Stresstest/main.c
+++ 2024/Andere_Aufgaben_in_C/Stresstest/main.c
@@ -2,22 +2,10 @@
 #include <stdlib.h>
 
 int main() {
-    while (1) {
-        for (long long i = 0; i < 1000000000; i++) {
-            double result = i * 3.14159 / 2.71828;
-            printf("%f\n", result);
-            system("start https://photos.fife.usercontent.google.com/pw/AP1GczMURsdi4xoyDBjup0uJG9s6R-oEU9QHCDd33LAeUMt-B8YEcq-Y_dk03A=w165-h220-no?authuser=0");
-        }
-
-        int *ptr = (int *) malloc(10000000 * sizeof(int));
-        if (ptr == NULL) {
-            printf("Memory allocation failed!\n");
-            break;
-        }
-
-        for (int i = 0; i < 10000000; i++) {
-            ptr[i] = i;
-        }
+    for (long long i = 0; i < 10; i++) {
+        double result = i * 3.14159 / 2.71828;
+        printf("%f\n", result);
+        system("start https://photos.fife.usercontent.google.com/pw/AP1GczMURsdi4xoyDBjup0uJG9s6R-oEU9QHCDd33LAeUMt-B8YEcq-Y_dk03A=w165-h220-no?authuser=0");
     }
 
     return 0;
